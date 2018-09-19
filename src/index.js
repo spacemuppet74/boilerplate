6b8641a303edb0c035d4cea93fddc4e3896c5fbf
Stefen Hasselberg <stefen74@gmail.com>
2018-09-19 12:24:55 +1200

    new boilerplate project with Babel 7

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = document.querySelector("#app");
+
+import "./index.scss";
+
+ReactDOM.render(<h1>Hello World###</h1>, el);
